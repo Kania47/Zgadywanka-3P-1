60983db95b12ad155d4925793b1da686b8a1eeaf
Kania47 <skilllu11@gmail.com>
2024-09-04 15:46:34 +0200

    Dodatkowy Komunikat

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     //nazwa pliku i nazwa klasy muszą być takie same
     //Nazwy klas powinny być z wielkiej litery, gdyż tak kaze obyczaj
     public static void main(String[] args) {
-        System.out.println("Hello world!");}
+        System.out.println("Hello world!");
+
+        System.out.println("Witamy na lekcjach programowania");
+    }
+
 }
+
+
