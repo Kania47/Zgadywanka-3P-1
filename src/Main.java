4194894c8618f708dbb9912889253b69ea7357a7
Kania47 <skilllu11@gmail.com>
2024-09-04 15:22:14 +0200

    pusty projekt

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    //nazwa pliku i nazwa klasy muszą być takie same
+    //Nazwy klas powinny być z wielkiej litery, gdyż tak kaze obyczaj
+    public static void main(String[] args) {
+        System.out.println("Hello world!");}
+}
